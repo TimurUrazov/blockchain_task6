6dae066b51b9aa6b525477d0037418b6559079b8
TimurUrazov <tu177086@gmail.com>
2022-12-30 19:09:17 +0300

    change compiler version

--- contracts/FlashloanCyclicalSwap.sol
+++ contracts/FlashloanCyclicalSwap.sol
@@ -1,5 +1,5 @@
 // // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.6.6 <0.8.0;
+pragma solidity =0.6.6;
 
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Callee.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
